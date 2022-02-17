44f1c09dc7f038ed7df9aca992b84a5776dcc73c
FarhanSafin <farhansafin@gmail.com>
2022-02-17 10:51:16 +0600

    Saving part done

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ function gettingValue(val, text) {
     
 }
 
+function gettingValueFromText(val, text) {
+    const inputField = document.getElementById(val);
+    const valueNumber = parseFloat(inputField.innerText);
+
+    const inputOf = document.getElementById(text);
+    const inputOfText = inputOf.innerText;
+
+    if(valueNumber >= 0) {
+        return valueNumber;
+    }else{
+        alert('Give proper and positive value for: ' + inputOfText);
+    }
+    
+}
+
 function setingValue(fieldId, balance){
     const getValue = document.getElementById(fieldId);
     if (balance >= 0){
@@ -52,9 +67,15 @@ document.getElementById('calculate-button').addEventListener('click', function()
 document.getElementById('save-button').addEventListener('click', function(){
     const incomeNumber = gettingValue('income-field', 'income-text');
 
+    const remainBalanceNumber = gettingValueFromText('remaining-balance', 'balance-text');
+
     const savingNumber = gettingValue('save-field', 'save-text');
 
     const saving = incomeNumber * (savingNumber/100);
 
+    const remainAfterSave = remainBalanceNumber - saving;
+
     setingValue('saved-amount', saving);
+
+    setingValue('remain-after-save', remainAfterSave);
 })
