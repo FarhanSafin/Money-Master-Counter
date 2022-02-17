1bd879d799c0dcef3338d567e7569d3bfa27bb54
FarhanSafin <farhansafin@gmail.com>
2022-02-17 12:38:30 +0600

    All done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* Get values from the input fields */
 function gettingValue(val, text) {
     const inputField = document.getElementById(val);
     const valueNumber = parseFloat(inputField.value);
@@ -13,32 +14,18 @@ function gettingValue(val, text) {
     
 }
 
-function gettingValueFromText(val, text) {
-    const inputField = document.getElementById(val);
-    const valueNumber = parseFloat(inputField.innerText);
-
-    const inputOf = document.getElementById(text);
-    const inputOfText = inputOf.innerText;
-
-    if(valueNumber >= 0) {
-        return valueNumber;
-    }else{
-        alert('Give proper and positive value for: ' + inputOfText + ' Please try again.');
-    }
-    
-}
-
+/* Setting value in the page */
 function setingValue(fieldId, balance){
     const getValue = document.getElementById(fieldId);
     if (balance >= 0){
         getValue.innerText = balance;
     }else{
-        /* getValue.innerText = "Can not calculate. Invalid Information"; */
         window.location.reload();
     }
     
 }
 
+/* Get necessary value and calculate for expense and remaining balance */
 document.getElementById('calculate-button').addEventListener('click', function() {
     const incomeNumber = gettingValue('income-field', 'income-text');
 
@@ -49,38 +36,38 @@ document.getElementById('calculate-button').addEventListener('click', function()
     const clothesCostNumber = gettingValue('clothes-cost-field', 'clothes-text');
 
     const totalExpense = foodCostNumber + rentCostNumber + clothesCostNumber;
+    const fixedtotalExpense = totalExpense.toFixed(3);
 
     const remainingBalance = incomeNumber - totalExpense;
+    const fixedremainingBalance = remainingBalance.toFixed(3);
 
     if (totalExpense > incomeNumber) {
         alert('You can not spend more than your income. Please try again.');
         window.location.reload();
     }
     else{
-        setingValue('expense', totalExpense);
-        setingValue('remaining-balance', remainingBalance);
+        setingValue('expense', fixedtotalExpense);
+        setingValue('remaining-balance', fixedremainingBalance);
     }
-    
+
 })
 
-
-
+/* Calculation for finding the save amount */
 document.getElementById('save-button').addEventListener('click', function(){
     const incomeNumber = gettingValue('income-field', 'income-text');
 
-    const remainBalanceNumber = gettingValueFromText('remaining-balance', 'balance-text');
+    const remainBalanceNumber = document.getElementById('remaining-balance');
+    const remainBalanceNumberFloat = parseFloat(remainBalanceNumber.innerText)
 
     const savingNumber = gettingValue('save-field', 'save-text');
 
     let saving = incomeNumber * (savingNumber/100);
-    let fixedSaving = saving.toFixed(2)
+    let fixedSaving = saving.toFixed(3)
 
-    let remainAfterSave = remainBalanceNumber - saving;
-    let fixedRemainAfterSave = remainAfterSave.toFixed(2)
+    let remainAfterSave = remainBalanceNumberFloat - saving;
+    let fixedRemainAfterSave = remainAfterSave.toFixed(3)
 
-    
-
-    if (saving > remainBalanceNumber){
+    if (saving > remainBalanceNumberFloat){
         alert('You dont have enough money to save. Please try again.');
         window.location.reload();
     }
